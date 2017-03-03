4faa83a98d7fe364b455d2d44e669d2df8a07760
Blay93 <sergioblay93@outlook.com>
2017-03-03 11:10:50 +0100

    Added login restriction
    A logged user cannot go to login page

--- appSpring/src/main/java/appSpring/controller/LoginController.java
+++ appSpring/src/main/java/appSpring/controller/LoginController.java
@@ -1,5 +1,7 @@
 package appSpring.controller;
 
+
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,8 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class LoginController {
 	
 	@RequestMapping("/login")
-	public String user(Model model){
+	public String user(Model model,HttpServletRequest request){
 		//model.addAttribute();
+		if(request.isUserInRole("ADMIN")||request.isUserInRole("USER")) return "forward:/";
 		return "login";
 	}
 }
