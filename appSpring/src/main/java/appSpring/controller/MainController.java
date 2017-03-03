0149dca9dc19781f0e8a63bec9d7a2dd85d3aeed
cvazquezlos <c.vazquezlosada@gmail.com>
2017-03-03 18:16:39 +0100

    Pages problem solved.

--- appSpring/src/main/java/appSpring/controller/MainController.java
+++ appSpring/src/main/java/appSpring/controller/MainController.java
@@ -1,14 +1,17 @@
 package appSpring.controller;
 
 import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import appSpring.entity.User;
 import appSpring.repository.ResourceRepository;
 import appSpring.repository.ResourceTypeRepository;
+import appSpring.repository.UserRepository;
 
 @Controller
 public class MainController {
@@ -17,19 +20,50 @@ public class MainController {
 	private ResourceRepository resourceRepository;
 	@Autowired
 	private ResourceTypeRepository resourceTypeRepo;
+	@Autowired
+	private UserRepository userRepository;
 
 	@PostConstruct
 	public void init() {
 	}
 
 	@RequestMapping("/")
-	public String books(Model model) {
+	public String resources(Model model, HttpServletRequest request) {
 		
+		if (request.isUserInRole("ADMIN") || request.isUserInRole("USER")) {
+			User loggedUser = userRepository.findByName(request.getUserPrincipal().getName());
+			model.addAttribute("user", loggedUser);
+		}
+		model.addAttribute("index", true);
 		model.addAttribute("books", resourceRepository.findByResourceType(resourceTypeRepo.findOneByName("Libro")));
 		model.addAttribute("magazines", resourceRepository.findByResourceType(resourceTypeRepo.findOneByName("Revista")));
 		model.addAttribute("all", resourceRepository.findAll());
 
 		return "index";
+	}
+
+	@RequestMapping("/about")
+	public String about(Model model, HttpServletRequest request) {
+
+		if (request.isUserInRole("ADMIN") || request.isUserInRole("USER")) {
+			User loggedUser = userRepository.findByName(request.getUserPrincipal().getName());
+			model.addAttribute("user", loggedUser);
+		}
+		model.addAttribute("about", true);
+
+		return "about";
+	}
+
+	@RequestMapping("/contact")
+	public String contact(Model model, HttpServletRequest request) {
+
+		if (request.isUserInRole("ADMIN") || request.isUserInRole("USER")) {
+			User loggedUser = userRepository.findByName(request.getUserPrincipal().getName());
+			model.addAttribute("user", loggedUser);
+		}
+		model.addAttribute("contact", true);
+
+		return "contact";
 	}
 
 }
