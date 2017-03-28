66640620b2ed71de696f0a5b56216c193e9625cb
Blay93 <sergioblay93@outlook.com>
2017-03-28 22:12:28 +0200

    Added LoginRestController

---
+++ appSpring/src/main/java/appSpring/model/UserComponent.java
@@ -0,0 +1,26 @@
+package appSpring.model;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.WebApplicationContext;
+
+@Component
+@Scope(value = WebApplicationContext.SCOPE_SESSION, proxyMode = ScopedProxyMode.TARGET_CLASS)
+public class UserComponent {
+
+	private User user;
+
+	public User getLoggedUser() {
+		return user;
+	}
+
+	public void setLoggedUser(User user) {
+		this.user = user;
+	}
+
+	public boolean isLoggedUser() {
+		return this.user != null;
+	}
+
+}
