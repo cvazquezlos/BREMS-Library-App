bf8475682cb1655b7a4ab9ddfc7b96b2221639fb
cvazquezlos <c.vazquezlosada@gmail.com>
2017-04-11 23:16:42 +0200

    Loan information correctly shown in profile.

--- frontend/src/app/model/action.model.ts
+++ frontend/src/app/model/action.model.ts
@@ -1,6 +1,9 @@
+import {ResourceCopy} from './resource-copy.model';
+
 export interface Action {
   id?: number;
   dateLoanInit: any;
   dateLoanGiven: any;
   dateLoanReturn: any;
+  copy: ResourceCopy;
 }
