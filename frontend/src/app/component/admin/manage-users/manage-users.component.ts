a951a08f0b8ce52d84cc47d255efec7f9406de05
cvazquezlos <c.vazquezlosada@gmail.com>
2017-04-18 00:01:48 +0200

    Solved some errors, added some features and...
    ... implemented user's administration but not tested.

--- frontend/src/app/component/admin/manage-users/manage-users.component.ts
+++ frontend/src/app/component/admin/manage-users/manage-users.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+
+import {User} from '../../../model/user.model';
+
+import {SessionService} from '../../../service/session.service';
+import {UserService} from '../../../service/user.service';
 
 @Component({
   selector: 'app-manage-users',
@@ -7,9 +13,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManageUsersComponent implements OnInit {
 
-  constructor() { }
+  errorMessage: boolean;
+  message: String;
+  successMessage: boolean;
+  users: User[];
 
-  ngOnInit() {
+  constructor(private router: Router, private userService: UserService, private sessionService: SessionService) {
+    this.successMessage = false;
+    this.errorMessage = false;
   }
 
+  ngOnInit() {
+    if (!this.sessionService.checkCredentials()) {
+      this.router.navigate(['/login']);
+    } else {
+      this.users = this.userService.getAllUsers();
+    }
+  }
+
+  delete(id: number) {
+    this.userService.deleteUser(id).subscribe(
+      response => {
+        this.successMessage = true;
+        this.errorMessage = false;
+        this.message = 'Usuario eliminado correctamente.';
+        console.log('User successfully deleted.');
+      },
+      error => {
+        this.successMessage = false;
+        this.errorMessage = true;
+        this.message = 'No se ha podido eliminar al usuario. Tiene un préstamo o una multa activa.'
+        console.log('Fail trying to delete selected user.');
+      }
+    );
+  }
 }
