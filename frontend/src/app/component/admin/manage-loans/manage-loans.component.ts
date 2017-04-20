f20d553b46e067919eb68e8e318753f770b0f93e
jherel <jhereldev@gmail.com>
2017-04-20 13:09:22 +0200

    Add pagination in manage-loans, and delete loan

--- frontend/src/app/component/admin/manage-loans/manage-loans.component.ts
+++ frontend/src/app/component/admin/manage-loans/manage-loans.component.ts
@@ -7,27 +7,111 @@ import { SessionService } from 'app/service/session.service';
 import { ActionService } from 'app/service/action.service'
 
 @Component({
-  templateUrl: './manage-loans.component.html',
-  styleUrls: ['./manage-loans.component.css']
+    templateUrl: './manage-loans.component.html',
+    styleUrls: ['./manage-loans.component.css']
 })
 export class ManageLoansComponent implements OnInit {
 
-  private loans: Action[] = [];
+    private loans: Action[] = [];
+    private errorMessage: boolean;
+    private message: String;
+    private showNextPage: boolean;
+    private showPreviousPage: boolean;
+    private successMessage: boolean;
+    private loansPage: number;
 
-  constructor(private router: Router,
-    private sessionService: SessionService,
-    private actionService: ActionService) {
-  }
-
-  ngOnInit() {
-    if (!this.sessionService.checkCredentials()) {
-      this.router.navigate(["/login"]);
-    } else {
-      this.actionService.getAllActions().subscribe(
-        loans => { this.loans = loans; console.log(loans); },
-        error => console.log(error)
-      );
+    constructor(private router: Router,
+        private sessionService: SessionService,
+        private actionService: ActionService) {
+        this.successMessage = false;
+        this.errorMessage = false;
+        this.loansPage = 0;
+        this.showNextPage = false;
+        this.showPreviousPage = false;
     }
-  }
+
+    ngOnInit() {
+        if (!this.sessionService.checkCredentials()) {
+            this.router.navigate(["/login"]);
+        } else {
+            this.getLoans();
+            this.checkNextPage();
+            this.checkPreviousPage();
+        }
+    }
+
+    getLoans() {
+        this.actionService.getAllActions(this.loansPage).subscribe(
+            loans => this.loans = loans,
+            error => console.log(error)
+        );
+    }
+
+    nextPage() {
+        this.showNextPage = false;
+        this.showPreviousPage = false;
+        this.loansPage++;
+        this.getLoans();
+        this.checkNextPage();
+        this.showPreviousPage = true;
+    }
+
+    previousPage() {
+        this.showNextPage = false;
+        this.showPreviousPage = false;
+        this.loansPage--;
+        this.getLoans();
+        this.checkPreviousPage();
+        this.showNextPage = true;
+    }
+
+    checkNextPage() {
+        this.actionService.getAllActions(this.loansPage + 1).subscribe(
+            loans => {
+                if (Object.keys(loans).length === 0) {
+                    this.showNextPage = false;
+                } else {
+                    this.showNextPage = true;
+                }
+            }
+        );
+    }
+
+    checkPreviousPage() {
+        if (this.loansPage > 0) {
+            this.actionService.getAllActions(this.loansPage - 1).subscribe(
+                loans => {
+                    if (Object.keys(loans).length === 0) {
+                        this.showPreviousPage = false;
+                    } else {
+                        this.showPreviousPage = true;
+                    }
+                }
+            );
+        } else {
+            this.showPreviousPage = false;
+        }
+    }
+
+    deleteLoan(id: number) {
+        this.actionService.deleteAction(id).subscribe(
+            response => {
+                this.successMessage = true;
+                this.errorMessage = false;
+                this.message = 'Prestamo eliminado correctamente.';
+                this.loansPage = 0;
+                console.log('Loan successfully deleted.');
+                this.getLoans();
+                this.checkNextPage();
+                this.checkPreviousPage();
+            },
+            error => {
+                this.successMessage = false;
+                this.errorMessage = true;
+                this.message = 'No se ha podido eliminar el prestamo.'
+                console.log('Fail trying to delete selected loan.');
+            }
+        );
+    }
 
 }
--- frontend/src/app/service/action.service.ts
+++ frontend/src/app/service/action.service.ts
@@ -9,35 +9,51 @@ import { Action } from "../model/action.model";
 @Injectable()
 export class ActionService {
 
-  authCreds: string;
+    authCreds: string;
 
-  constructor(private http: Http) {
-  }
+    constructor(private http: Http) {
+    }
 
-  setAuthHeaders(authCreds: string) {
-    this.authCreds = authCreds;
-  }
+    setAuthHeaders(authCreds: string) {
+        this.authCreds = authCreds;
+    }
 
-  getAllActions(page?: number, finished?: boolean) {
-    this.authCreds = localStorage.getItem("creds");
+    getAllActions(page?: number, finished?: boolean) {
+        this.authCreds = localStorage.getItem("creds");
 
-    let headers: Headers = new Headers();
-    headers.append('Authorization', 'Basic ' + this.authCreds);
+        let headers: Headers = new Headers();
+        headers.append('Authorization', 'Basic ' + this.authCreds);
 
-    let url = (page && finished) ? ACTION_URL + '?page=' + page + '&finished=' + finished : ACTION_URL;
-    return this.http.get(url, { headers: headers })
-      .map(response => response.json().content)
-      .catch(error => Observable.throw('Server error'));
-  }
+        let url = ACTION_URL;
+        if (page)
+            url += '?page=' + page;
+        if (finished)
+            url += '&finished=' + finished;
 
-  postAction(action: Action) {
-    let body = JSON.stringify(action);
-    let headers: Headers = new Headers();
-    headers.append('Content-Type', 'application/json');
-    headers.append('X-Requested-With', 'XMLHttpRequest');
-    headers.append('Authorization', 'Basic ' + this.authCreds);
-    return this.http.post(ACTION_URL, body, { headers: headers })
-      .map(response => response.json())
-      .catch(error => Observable.throw('Server error'));
-  }
+        return this.http.get(url, { headers: headers })
+            .map(response => response.json().content)
+            .catch(error => Observable.throw('Server error'));
+    }
+
+    postAction(action: Action) {
+        let body = JSON.stringify(action);
+        let headers: Headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+        headers.append('X-Requested-With', 'XMLHttpRequest');
+        headers.append('Authorization', 'Basic ' + this.authCreds);
+        return this.http.post(ACTION_URL, body, { headers: headers })
+            .map(response => response.json())
+            .catch(error => Observable.throw('Server error'));
+    }
+
+    deleteAction(id: number) {
+        this.authCreds = localStorage.getItem("creds");
+        let headers: Headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+        headers.append('X-Requested-With', 'XMLHttpRequest');
+        headers.append('Authorization', 'Basic ' + this.authCreds);
+        return this.http.delete(ACTION_URL + '/' + id, { headers: headers })
+            .map(response => response.json())
+            .catch(error => Observable.throw('Server error'));
+    }
 }
